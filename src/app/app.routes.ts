09e039a3d8b312cf4b3feb5b89756a82cbb25bd0
BrunoAlmonacid <almonacidbrunosantiago@gmail.com>
2025-06-12 20:41:57 -0300

    login, registro y enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Products } from './pages/products/products';
 import { Cart } from './pages/cart/cart';
+import { Register } from './pages/auth/register/register';
+import { Login } from './pages/auth/login/login';
 
 
 export const routes: Routes = [
@@ -9,5 +11,7 @@ export const routes: Routes = [
     {path: 'home', component: Home},
     {path: 'products', component: Products},
     {path: 'cart', component: Cart},
+    {path: 'register', component: Register},
+    {path: 'login', component: Login},
     {path: '**', redirectTo: '/home'}
 ];
