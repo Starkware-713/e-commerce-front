3762cfa03d83dba8d77257c08b42ef5b7f8f8817
Walter Carrasco <carrascowalter440@gmail.com>
2025-06-13 15:18:09 -0300

    paginas de errores 404 & 403

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,10 @@ import { Products } from './pages/products/products';
 import { Cart } from './pages/cart/cart';
 import { Register } from './pages/auth/register/register';
 import { Login } from './pages/auth/login/login';
-
+import { Privacy } from './pages/privacy/privacy';
+import { Terms } from './pages/terms/terms';
+import { NotFound } from './pages/errors/not-found/not-found';
+import { Forrbiden } from './pages/errors/forrbiden/forrbiden';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -13,5 +16,9 @@ export const routes: Routes = [
     {path: 'cart', component: Cart},
     {path: 'register', component: Register},
     {path: 'login', component: Login},
+    {path: 'privacy', component: Privacy},
+    {path: 'terms', component: Terms},
+    {path: '404', component: NotFound},
+    {path: '403', component: Forrbiden},
     {path: '**', redirectTo: '/home'}
 ];
